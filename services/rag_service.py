9073a5cc33f00b402013584a91b34ff7c0d74d76
Tom <tyohin2016@163.com>
2025-12-08 14:25:34 +1300

    service layer: pass the conversation id and question to service layer and conducted below steps: 1.get chat_history from db 2.call AI model and pass the history and question to AI model and return answer and new context 3.save them in db 4.return JSON

--- services/rag_service.py
+++ services/rag_service.py
@@ -2,10 +2,10 @@ from repositories import rag_repository
 from rag_core.rag_pipeline import run_rag
 
 def process_question(conversation_id: str, question: str):
-    history = rag_repository.get_history(conversation_id)
-    answer, context = run_rag(question, history)
-    rag_repository.save_message(conversation_id, "用户", question)
-    rag_repository.save_message(conversation_id, "助手", answer)
+    history = rag_repository.get_history(conversation_id) #从数据库中获取对话历史
+    answer, context = run_rag(question, history) #这里调用大模型 传入对话历史以及问题 得到答案以及新的上下文
+    rag_repository.save_message(conversation_id, "User", question)  #将对话历史存入
+    rag_repository.save_message(conversation_id, "Assistant", answer) #将对话历史存入
     return {
         "conversation_id": conversation_id,
         "answer": answer,
