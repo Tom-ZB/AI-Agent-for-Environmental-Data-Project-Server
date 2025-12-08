bf2f92a9e8ed4cb42885aab0993be501f99932b6
Tom <tyohin2016@163.com>
2025-12-08 13:50:14 +1300

    use sqlite to manipulate the database

--- repositories/rag_repository.py
+++ repositories/rag_repository.py
@@ -9,8 +9,9 @@ def save_message(conversation_id: str, role: str, content: str):
     db.commit()
     db.close()
 
+# Get chat_history from database
 def get_history(conversation_id: str):
-    db: Session = SessionLocal()
+    db: Session = SessionLocal()  #获取Session实例 来操作数据库
     messages = db.query(ConversationHistory).filter(ConversationHistory.conversation_id == conversation_id).all()
     db.close()
     return "\n".join([f"{m.role}: {m.content}" for m in messages]) if messages else "（暂无历史）"
